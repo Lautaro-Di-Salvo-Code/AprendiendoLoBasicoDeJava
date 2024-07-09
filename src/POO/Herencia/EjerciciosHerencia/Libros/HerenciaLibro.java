e4f3f7d38ec3d4ab2475bf2c57d6aacf42a35761
Lauta Di Salvo <lautarodisalvo2023@gmail.com>
2024-07-09 12:43:03 -0300

    algunas cosas mas

--- src/POO/Herencia/EjerciciosHerencia/Libros/HerenciaLibro.java
+++ src/POO/Herencia/EjerciciosHerencia/Libros/HerenciaLibro.java
@@ -4,11 +4,6 @@ public class HerenciaLibro extends ItemBiblioteca{//hereda los valores de ItemBi
 
         private String autor;
         private int Cantidad_paginas;//y estira la informacion segun los requirimientos
-        public HerenciaLibro(String autor,int Cantidad_paginas){
-            super();//esto es herencia, 
-            this.autor = autor;
-            this.Cantidad_paginas = Cantidad_paginas;
-        }
         public String LeerMasInfoLibro(){
             return "El autor del libro es: " + this.autor +"\n el libro tiene "+ this.Cantidad_paginas+ " paginas";
         }
